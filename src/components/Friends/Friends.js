476e27a58f3fd53a6a30d96dd146b9cda0e54f0b
nibir <biplob.asanibir@gmail.com>
2020-03-13 09:17:02 +0600

    add friend section desgin

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Friend.css'
 
 const Friends = (props) => {
     const friends = props.friends
     const friendIncome = friends.reduce(((total, friend) => Number(total)+Number(friend.income)),0 )
     console.log(friendIncome)
     return (
-        <div>
+        <div className="friend">
             <h3>Total: {friends.length}</h3>
             <h3>Total Income: {friendIncome}</h3>
         </div>
