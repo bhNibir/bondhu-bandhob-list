1e609e2ffa6bc4669c67460c1b1a0d37b1124afc
nibir <biplob.asanibir@gmail.com>
2020-03-13 10:05:55 +0600

    add friend section desgin

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -4,7 +4,7 @@ import './Friend.css'
 const Friends = (props) => {
     const friends = props.friends
     const friendIncome = friends.reduce(((total, friend) => Number(total)+Number(friend.income)),0 )
-    console.log(friendIncome)
+    
     return (
         <div className="friend">
             <h3>Total: {friends.length}</h3>
