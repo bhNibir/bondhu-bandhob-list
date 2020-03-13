1b273f3a35eadf20fa01d1afae4492d43645f54b
nibir <biplob.asanibir@gmail.com>
2020-03-13 21:42:29 +0600

    added friend list in sidebar

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -5,10 +5,27 @@ const Friends = (props) => {
     const friends = props.friends
     const friendIncome = friends.reduce(((total, friend) => Number(total)+Number(friend.income)),0 )
     
+    
+    
     return (
-        <div className="friend">
-            <h3>Total Friends: {friends.length}</h3>
-            <h3>Total Income: {friendIncome}</h3>
+        <div className="friend-container">
+            <div className="total-friend">
+                <h3>Total Friends: {friends.length}</h3>
+                <h3>Total Income: {friendIncome}</h3>
+                <table>
+                {
+                    friends.map(user => <React.Fragment>
+                        <tr>
+                            <td><img src={user.img} alt=""/></td>
+                            <td>{user.name}</td>
+                        </tr>
+                    </React.Fragment>)
+                }
+                </table>
+
+                
+            </div>
+            
         </div>
     );
 };
